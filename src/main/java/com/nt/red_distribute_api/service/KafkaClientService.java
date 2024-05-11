70876b061bc77c9024f3f4535513eb6eaf3079da
คนที่คุณก็รู้ว่าใคร <pawat@computerlogy.com>
2024-05-11 16:23:10 +0700

    update host kafka

--- src/main/java/com/nt/red_distribute_api/service/KafkaClientService.java
+++ src/main/java/com/nt/red_distribute_api/service/KafkaClientService.java
@@ -56,28 +56,46 @@ import org.apache.kafka.common.resource.PatternType;
 import org.apache.kafka.common.resource.ResourcePattern;
 import org.apache.kafka.common.resource.ResourcePatternFilter;
 import org.apache.kafka.common.resource.ResourceType;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.nt.red_distribute_api.dto.req.kafka.TopicReq;
 import com.nt.red_distribute_api.dto.resp.UserAclsInfo;
 
+import jakarta.annotation.PostConstruct;
+
 @Service
 public class KafkaClientService {
+
+    @Value("${kafka.bootstrap.server}")
+    private String bootstrapServer;
+
     private AdminClient client = null;
 
     private final Object lock = new Object();
 
     private Properties props = new Properties();
 
-    public KafkaClientService() {
-        // Ideally, you would import these settings from a properties file or the like
-        // props.setProperty("ssl.endpoint.identification.algorithm", "https");
-        props.setProperty(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "34.142.215.79:9092,34.142.251.254:9092");
+    @PostConstruct
+    public void init() {
+        // Initialize props after the value of bootstrapServer has been injected
+        props.setProperty(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
         props.setProperty("security.protocol", "SASL_PLAINTEXT");
         props.setProperty("sasl.mechanism", "SCRAM-SHA-256");
         props.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"admin\" password=\"admin-secret\";");
         client = AdminClient.create(props);
     }
+
+    // public KafkaClientService() {
+    //     // Ideally, you would import these settings from a properties file or the like
+    //     // props.setProperty("ssl.endpoint.identification.algorithm", "https");
+    //     // props.setProperty(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "34.142.215.79:9092,34.142.251.254:9092");
+    //     props.setProperty(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+    //     props.setProperty("security.protocol", "SASL_PLAINTEXT");
+    //     props.setProperty("sasl.mechanism", "SCRAM-SHA-256");
+    //     props.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"admin\" password=\"admin-secret\";");
+    //     client = AdminClient.create(props);
+    // }
     public void teardown() {
         client.close();
     }
