312a052f129e4a89bb7ef0672a455a53c1aed25f
คนที่คุณก็รู้ว่าใคร <pawat@computerlogy.com>
2024-05-19 12:47:20 +0700

    init jboss success

--- src/main/java/com/nt/red_distribute_api/RedDistributeApiApplication.java
+++ src/main/java/com/nt/red_distribute_api/RedDistributeApiApplication.java
@@ -11,12 +11,10 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class RedDistributeApiApplication extends SpringBootServletInitializer{
 
-	static {
-        // Remove existing handlers attached to the j.u.l root logger
-        java.util.logging.LogManager.getLogManager().reset();
-        // Bridge JUL to SLF4J
-        SLF4JBridgeHandler.install();
-    }
+	// static {
+    //     // Bridge JUL to SLF4J
+    //     SLF4JBridgeHandler.install();
+    // }
 	public static void main(String[] args) {
         SpringApplication.run(RedDistributeApiApplication.class, args);
     }
