981865d3fba201e7632f4c8762f378aaa8216545
คนที่คุณก็รู้ว่าใคร <pawat@computerlogy.com>
2024-06-11 11:02:35 +0700

    test deploy

--- src/main/java/com/nt/red_distribute_api/entity/view/permission/ListPermissionTotalUser.java
+++ src/main/java/com/nt/red_distribute_api/entity/view/permission/ListPermissionTotalUser.java
@@ -4,44 +4,13 @@ package com.nt.red_distribute_api.entity.view.permission;
 import java.sql.Clob;
 import java.sql.Timestamp;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Entity
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@ToString
-@Table (name = "sa_menu_permission")
-public class ListPermissionTotalUser {
-        @Id
-        @Column(name = "ID")
-        private Long id;
-
-        @Column(name = "PERMISSION_NAME", unique = false,nullable = true)
-        private String permission_Name=null;
-
-        @Column(name = "PERMISSION_JSON", unique = false,nullable = true)
-        private Clob permission_json=null;
-
-        @Column(name = "CREATED_DATE", unique = false,nullable = true)
-        private Timestamp created_Date=null;
-        
-        @Column(name = "CREATED_BY", unique = false,nullable = true)
-        private String created_By=null;
-
-        @Column(name = "UPDATED_DATE", unique = false,nullable = true)
-        private Timestamp updated_Date=null;
-
-        @Column(name = "UPDATED_BY", unique = false,nullable = true)
-        private String updated_By=null;
-
-        @Transient
-        @Column(name = "TOTALUSER", unique = false,nullable = true)
-        private Integer totalUser=0;
+public interface ListPermissionTotalUser {
+        Long getID();
+        String getPERMISSION_NAME();
+        Clob getPERMISSION_JSON();
+        Timestamp getCREATED_DATE();
+        String getCREATED_BY();
+        Timestamp getUPDATED_DATE();
+        String getUPDATED_BY();
+        Integer gettotalUser();
 }
