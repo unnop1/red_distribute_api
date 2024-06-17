56f6fd988671ba1a64eaa9ada2a6b2d5844bbc95
คนที่คุณก็รู้ว่าใคร <pawat@computerlogy.com>
2024-06-17 14:46:00 +0700

    test deploy

--- src/main/java/com/nt/red_distribute_api/entity/TriggerMessageEntity.java
+++ src/main/java/com/nt/red_distribute_api/entity/TriggerMessageEntity.java
@@ -46,9 +46,9 @@ public class TriggerMessageEntity {
         @Column(name = "IS_STATUS", unique = false,nullable = true)
         private Integer IS_STATUS = null;
 
-        @JsonBackReference
+        @Lob
         @Column(name = "MESSAGE_IN", unique = false,nullable = true)
-        private Clob MESSAGE_IN = null;
+        private String MESSAGE_IN = null;
 
         @Lob
         @Column(name = "DATA_MODEL", unique = false,nullable = true)
--- src/main/java/com/nt/red_distribute_api/entity/view/permission/ListPermissionTotalUser.java
+++ src/main/java/com/nt/red_distribute_api/entity/view/permission/ListPermissionTotalUser.java
@@ -16,32 +16,32 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-@Table (name = "sa_menu_permission")
+@Table (name = "SA_MENU_PERMISSION", schema = "${replace_schema}")
 public class ListPermissionTotalUser {
         @Id
-        @Column(name = "id")
+        @Column(name = "ID")
         private Long id;
 
-        @Column(name = "permission_Name", unique = false,nullable = true)
+        @Column(name = "PERMISSION_NAME", unique = false,nullable = true)
         private String permission_Name=null;
 
         @Lob
-        @Column(name = "permission_json", unique = false,nullable = true)
+        @Column(name = "PERMISSION_JSON", unique = false,nullable = true)
         private String permission_json=null;
 
-        @Column(name = "created_Date", unique = false,nullable = true)
+        @Column(name = "CREATED_DATE", unique = false,nullable = true)
         private Timestamp created_Date=null;
         
-        @Column(name = "created_By", unique = false,nullable = true)
+        @Column(name = "CREATED_BY", unique = false,nullable = true)
         private String created_By=null;
 
-        @Column(name = "updated_Date", unique = false,nullable = true)
+        @Column(name = "UPDATED_DATE", unique = false,nullable = true)
         private Timestamp updated_Date=null;
 
-        @Column(name = "updated_By", unique = false,nullable = true)
+        @Column(name = "UPDATED_BY", unique = false,nullable = true)
         private String updated_By=null;
 
         @Transient
-        @Column(name = "TotalUser", unique = false,nullable = true)
+        @Column(name = "TOTALUSER", unique = false,nullable = true)
         private Integer totalUser=0;
 }
