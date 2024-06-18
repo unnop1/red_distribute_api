81075e2e0abe2eef4833c3cc52867f2df5e771a1
คนที่คุณก็รู้ว่าใคร <pawat@computerlogy.com>
2024-06-18 14:27:58 +0700

    test deploy

--- src/main/java/com/nt/red_distribute_api/entity/view/permission/ListPermissionTotalUser.java
+++ src/main/java/com/nt/red_distribute_api/entity/view/permission/ListPermissionTotalUser.java
@@ -1,16 +1,15 @@
 package com.nt.red_distribute_api.entity.view.permission;
 
 
+import java.sql.Clob;
 import java.sql.Timestamp;
 
 import jakarta.persistence.Lob;
 
 public interface ListPermissionTotalUser {
         Long getID();
-        @Lob
         String getPERMISSION_NAME();
-        @Lob
-        String getPERMISSION_JSON();
+        Clob getPERMISSION_JSON();
         Timestamp getCREATED_DATE();
         String getCREATED_BY();
         Timestamp getUPDATED_DATE();
